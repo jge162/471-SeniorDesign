40ac1737c06351f31098cda130c97060243d5fa1
Jermyiah <31228460+jge162@users.noreply.github.com>
2023-01-29 23:58:07 -0800

    Adding team members
    Senior design project team members
    
    Co-Authored-By: Duy Nguyen <46938160+duy301199@users.noreply.github.com>
    Co-Authored-By: Andrew Takayama <54484154+AndrewTakayama@users.noreply.github.com>
    Co-Authored-By: Spencer Williams <95833009+5pencerW@users.noreply.github.com>

--- google_coral.py
+++ google_coral.py
@@ -1,43 +1,19 @@
 
-import pycoral.coral as coral
-import numpy as np
-import tensorflow as tf
+# python3
+import serial
 
-# Create a new Edge TPU device
-edgetpu_device = coral.Device()
-
-# Load the TensorFlow Lite model
-model = edgetpu_device.LoadTensorFlowModel("path/to/model.tflite")
-
-# Perform inferences
-input_tensor = model.get_input_tensor(0)
-output_tensor = model.get_output_tensor(0)
-results = model.RunInference(input_tensor)
-print(results)
+ser = serial.Serial('/dev/ttyACM0',9600) 
+ser.write(b'trash')
+ser.close() 
 
 
+# mdt push (file path here)
+# mdt shell (connect to coral)
+# ls -1
+# mkdir (create folder) used to move file to folder
+# mv files
+# cd filename
+# edgetpu_detect_server --model (.tflite file) --source (dev1 for camera) --labels (txt file)
 
-# Load the TensorFlow Lite model
-interpreter = tf.lite.Interpreter(model_path="path/to/model.tflite")
-interpreter.allocate_tensors()
 
-# Get input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-# Create input data
-input_data = np.array(...) # Replace with your input data
-
-# Set the input tensor
-interpreter.set_tensor(input_details[0]['index'], input_data)
-
-# Perform inferences
-interpreter.invoke()
-
-# Get the output tensor
-output_data = interpreter.get_tensor(output_details[0]['index'])
-
-print(output_data)
-
-scp model.tflite <username>@<ip-address-of-coral-dev-board>:/path/to/model.tflite
 
