176175d0d1b3d9e7b1cb109479a3178e34645050
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-20 13:51:03 -0800

    Update UPDATED_Arduino.py

--- UPDATED_Arduino.py
+++ UPDATED_Arduino.py
@@ -51,21 +51,21 @@ def main():
         for c in classes:
             class_label = labels.get(c.id, c.id)
             confidence = c.score
-            if class_label == 'Recycling' and confidence > 0.7:
+            if class_label == 'SodaCan' and confidence > 0.7:
                 ser = serial.Serial('/dev/ttyACM0',9600) 
                 time.sleep(2)  # pause python script for 2 seconds
                 ser.write(b'composting')
                 ser.close()
                 time.sleep(3)  # pause python script for 3 second to allow, boxes to reset
 
-            elif class_label == 'Waste' and confidence > 0.5:
+            elif class_label == 'Water Bottle' and confidence > 0.5:
                 ser = serial.Serial('/dev/ttyACM0',9600) 
                 time.sleep(2)  # pause python script for 2 seconds
                 ser.write(b'trash')
                 ser.close()
                 time.sleep(3)  # pause python script for 3 second to allow, boxes to reset
 
-            elif class_label == 'Compost' and confidence > 0.5:
+            elif class_label == 'Paper plate' and confidence > 0.5:
                 ser = serial.Serial('/dev/ttyACM0',9600) 
                 time.sleep(2)  # pause python script for 2 seconds
                 ser.write(b'compost')
