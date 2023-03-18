b0a874a37572384eefe97e9bd822879fde4d416c
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-03-18 03:09:19 -0700

    Update websream_coral.py

--- websream_coral.py
+++ websream_coral.py
@@ -18,12 +18,28 @@ from pycoral.utils import dataset
 from pycoral.adapters import common
 from pycoral.adapters import classify
 from flask import Flask, Response, render_template
+import serial
+import threading
+from periphery import GPIO
+from time import time, sleep
+
+"""
+# Set up the GPIO pin for the LED
+led = GPIO("/dev/gpiochip2", 9, "out")  # P16_out
+print("LED OK")
+# Initialize the ultrasonic sensor
+echo_pin = GPIO("/dev/gpiochip4", 12, "in")
+trigger_pin = GPIO("/dev/gpiochip4", 10, "out")
+print("Sensor OK")
+trigger_pin.write(False)
+sleep(2)
+"""
 
 # Specify the TensorFlow model, labels, and camera device
 script_dir = pathlib.Path(__file__).parent.absolute()
 model_file = os.path.join(script_dir, 'Senior/model_edgetpu.tflite')
 label_file = os.path.join(script_dir, 'Senior/labels.txt')
-device = 0
+device = 1
 width = 640
 height = 480
 
@@ -35,12 +51,21 @@ interpreter.allocate_tensors()
 cap = cv2.VideoCapture(device)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+# ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # Initialize the Flask app
 app = Flask(__name__)
+detected_message = ""  # empty string to hold messages
 
 
 def gen_frames():
+    # Call the setup function to initialize the GPIO pins and stepper motors.
+    # Loop over frames from the camera
+    # global message
+    global detected_message
+    camera_paused = False
+    pause_start_time = None  # Initialize pause_start_time to None
+
     while True:
         # Capture the current frame from the camera
         ret, frame = cap.read()
@@ -62,14 +87,55 @@ def gen_frames():
         for c in classes:
             class_label = labels.get(c.id, c.id)
             confidence = c.score
-
+            print('%s detected: = %.5f' % (class_label, confidence))
             if class_label == 'Base' and confidence > 0.80:
                 # Check if the camera is already paused
                 print("Base case here do nothing")
+                time.sleep(1)
+                detected_message = "Base case here, do nothing."
+
+            elif class_label == 'Waste' and confidence > 0.80:
+                # Check if the camera is already paused
+                print("Trigger Arduino for Waste")
+                if not camera_paused:
+                    # Pause the camera by setting the variable to True
+                    camera_paused = True
+                    # Trigger the Waste process
+                    # ser.write(b'trash')
+                    detected_message = "Trigger Arduino for Waste."
+                    time.sleep(3)
+                # Exit the loop to prevent multiple instances of triggering
+                break
 
             elif class_label == 'Recycling' and confidence > 0.80:
                 # Check if the camera is already paused
-                print("Trigger Arduino to recycle")
+                print("Trigger Arduino for recycle")
+                # print('%s detected trigger Arduino : = %.5f' % (class_label, confidence))
+                if not camera_paused:
+                    # Pause the camera by setting the variable to True
+                    camera_paused = True
+                    # Trigger the recycling process
+                    # ser.write(b'recycle')
+                    detected_message = "Trigger Arduino for recycling."
+                    time.sleep(3)
+
+                # Exit the loop to prevent multiple instances of triggering
+                break
+
+            elif class_label == 'Compost' and confidence > 0.80:
+                # Check if the camera is already paused
+                print("Compost Trigger Arduino ")
+                if not camera_paused:
+                    # Pause the camera by setting the variable to True
+                    camera_paused = True
+                    # Trigger the Compost process
+                    # ser.write(b'compost')
+                    detected_message = "Trigger Arduino for Compost."
+                    time.sleep(3)
+                # Exit the loop to prevent multiple instances of triggering
+                break
+
+            # time.sleep(0.5)
 
         # Convert the frame to JPG format
         ret, buffer = cv2.imencode('.jpg', frame)
@@ -78,6 +144,40 @@ def gen_frames():
         # Yield the frame to the Flask app
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        # If the camera is not paused, display the frame and check for user input
+        if not camera_paused:
+            # Exit on 'c' key
+            if cv2.waitKey(1) & 0xFF == ord('c'):
+                break
+
+        # If the camera is paused, wait for 3 seconds to resume
+        else:
+            # Wait for 3 seconds
+            if not pause_start_time:
+                pause_start_time = time.time()
+            elif time.time() - pause_start_time >= 3:
+                pause_start_time = None
+                camera_paused = False
+
+        # Exit on 'c' key
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            break
+
+    # Release the camera and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+    # ser.close()
+
+
+def message_stream():
+    global detected_message
+    while True:
+        # Stream the Message feed from the detection cases
+        if detected_message:
+            yield f'data: {detected_message}\n\n'
+            detected_message = ""  # empty string
+        time.sleep(0.25)
 
 
 @app.route('/')
@@ -93,9 +193,62 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route('/message_stream')
+def message_stream_route():
+    # Stream the Message feed from the detection cases
+    return Response(message_stream(), content_type='text/event-stream')
+
+
 if __name__ == '__main__':
     # Start the Flask app
     app.run(host='0.0.0.0', debug=False)
+
+
+"""
+while True:
+    # Send a 10us pulse to trigger the sensor
+    trigger_pin.write(True)
+    sleep(0.00001)
+    trigger_pin.write(False)
+    # print("Trigger setup")
+    # Wait for the echo pin to go high
+    pause_start_time = time()
+    while echo_pin.read() == 0:
+        if time() - pause_start_time > 1.0:
+            # If the echo pin doesn't go high within 1 second, break the loop
+            break
+    else:
+        # Record the start time if the echo pin went high
+        pulse_start = time()
+
+        # Wait for the echo pin to go low
+        while echo_pin.read() == 1:
+            pass
+
+        # Record the end time when the echo pin went low
+        pulse_end = time()
+
+        # Calculate the pulse duration and the distance
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+
+        # Check if a human is detected (distance < 100cm)
+        if distance >= 65 or distance == 0:
+            # Turn off the LED
+            led.write(False)
+            print("No human detected")
+        else:
+            # Turn on the LED
+            led.write(True)
+            print("Detected", distance, "cm")
+            # Call the setup function to initialize the GPIO pins and stepper motors.
+            # Loop over frames from the camera
+            camera_paused = False
+            pause_start_time = None  # Initialize pause_start_time to None
+            while True:
+                # Capture the current frame from the camera
+                ret, frame = cap.read()
+"""
 
 """ 
 <!DOCTYPE html>
