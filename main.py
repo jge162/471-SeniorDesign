110a97d02489030177bad7b2c1d2bff385145513
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-05-18 14:54:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
 
 # https://automaticwastesorter.ngrok.app/
-
+# ngrok http --domain=automaticwastesorter.ngrok.app 5000
 
 """
 git commands to use -> 
